ce594086e2ad610ac87749a3e134dc7ad47abfc1
unknown <kenanbiberovic020@gmail.com>
2022-08-09 00:08:36 +0200

    for loop

--- js/vezba1/domaci3/domaci3.js
+++ js/vezba1/domaci3/domaci3.js
@@ -1,7 +1,7 @@
 unos1 = Number(prompt("Insert price of article"));
 unos2 = Number(prompt("Insert selling price"));
 if (unos1 > unos2) {
-  racunica = unos1 - unos2;
+  racunica = unos2 - unos1;
   console.log("Profit je" + racunica);
 } else if (unos2 > unos1) {
   racunica = unos2 - unos1;
---
+++ js/vezba2/vezba2.js
@@ -0,0 +1,39 @@
+// sum = 0;
+// a = Number(prompt("Unesite broj"));
+// for (i = 0; i <= a; i++) {
+//   console.log(i);
+//   sum += i;
+// }
+
+// console.log(+sum);
+
+// sum=0;
+// avg=0;
+// for (i = 1;i <= 10; i++);
+// {
+//   unos = Number(prompt("Unesite broj"));
+//   sum+=unos;
+// }
+// console.log("zbir je"+sum);
+// console.log("prosek je"+avg/10);
+
+// a = Number(prompt("Unesite broj"));
+
+// for (i = 1; i <= a; i++) {
+//   console.log(i);
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+cube = 0;
+a = Number(prompt("Unesite broj:"));
+for (i = 0; i <= a; i++) {
+  console.log("Cube of" + i + "is" + i ** 3);
+}
