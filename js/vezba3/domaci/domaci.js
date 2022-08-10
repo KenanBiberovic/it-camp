ec68977dde0e4b386304b1cbbb26634546e7d938
unknown <kenanbiberovic020@gmail.com>
2022-08-10 22:06:36 +0200

    commit

--- js/vezba3/domaci/domaci.js
+++ js/vezba3/domaci/domaci.js
@@ -1,11 +1,15 @@
-unos = Number(prompt("Unesi broj"));
-brojevi = "";
-cifra = 1;
-for (i = 1; i <= unos; i++) {
-  space = "";
-  for (j = 1; j <= unos - i; j++) {
-    space += " ";
+brojac = 1;
+string = "";
+unos = Number(prompt("unos :"));
+for (let i = 1; i <= unos; i++) {
+  for (let j = 1; j <= unos - i; j++) {
+    string += " ";
   }
-  brojevi += i + " ";
-  console.log(space + brojevi);
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    string += brojac + " ";
+    brojac++;
+  }
+  string += "\n";
 }
+console.log(string);
--- js/vezba3/vezba3.js
+++ js/vezba3/vezba3.js
@@ -4,20 +4,60 @@
 //   string = string + i;
 //   console.log(string);
 // }
-
+//
 // unos = Number(prompt("Unos :"));
 // for (i = 1; i <= unos; i++) {
 //   for (j = 1; j <= i; j++) console.log(i);
 // }
 
-unos = Number(prompt("Unesi broj"));
-zvezdice = "";
+// unos = Number(prompt("Unesi broj"));
+// zvezdice = "";
 
-for (i = 1; i <= unos; i++) {
-  space = "";
-  for (j = 1; j <= unos - i; j++) {
-    space += " ";
-  }
-  zvezdice += "* ";
-  console.log(space + zvezdice);
+// for (i = 1; i <= unos; i++) {
+//   space = "";
+//   for (j = 1; j <= unos - i; j++) {
+//     space += " ";
+//   }
+//   zvezdice += "* ";
+//   console.log(space + zvezdice);
+// }
+
+// sum = 0;
+// for (i = 1; i <= 10; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
+//
+// unos = Number(prompt("Unesi broj:"));
+// avr = unos;
+// sum = 0;
+// for (i = 0; i <= unos; i++);
+// {
+//   sum += i;
+//   avr = sum / unos;
+//   console.log("zbir je :" + sum);
+//   console.log("AVRG JE :" + avr);
+// }
+//
+// cube = 0;
+// unos = Number(prompt("Unesi broj:"));
+// for (i = 0; i <= unos; i++) {
+//   cube = i * i * i;
+//   console.log("Number is " + i, "and cube of the " + i, "is " + cube);
+// }
+
+// unos1 = Number(prompt("Unesip prvi cinilac:"));
+// unos2 = Number(prompt("Unesi drugi cinilac:"));
+// mult = 0;
+// for (i = 0; i <= unos2; i++) {
+//   mult = unos1 * i;
+//   console.log(unos1 + " X " + i, "= " + mult);
+// }
+// 8. ^*^*^*^*^*^^*^*^*^
+
+unos = number(prompt("Unesite broj;"));
+sum = 0;
+odd = 0;
+if (unos % 2 == 1) {
+  console.log(+odd);
 }
